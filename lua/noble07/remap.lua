f8c84365f07c715b6197f5badd5b1a461e4df23e
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2023-04-01 19:59:21 -0500

    Added remap

--- lua/noble07/remap.lua
+++ lua/noble07/remap.lua
@@ -13,4 +13,8 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
